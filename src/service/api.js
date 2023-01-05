28695fac04e7b7b0ef383d477967aca441ffa14a
Joshua Anop <rolexy.inc@gmail.com>
2023-01-05 12:06:43 +0100

    Revert "Merge request from DhiWise"

--- src/service/api.js
+++
@@ -1,7 +0,0 @@
-import { apis } from "service";
-
-export const getUsers = (payload) =>
-  apis.get(
-    `https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users/${payload?.path?.id}=[]`,
-    payload
-  );
